bcf0f4fd448286df192965a0cfb7847440cafe48
zhangheng@allinmd.cn <zhangheng123>
2018-03-20 13:33:16 +0800

    小程序vue架构mpvue实现

---
+++ src/pages/case/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    测试模块
+  </div>
+</template>
+<script>
+  export default {
+    mounted: function () {
+      console.log("加载完成")
+    }
+  }
+</script>
+<style lang="scss">
+  .container {
+    height: 300px;
+    width: 535px;
+    background: #ff0000;
+  }
+</style>
